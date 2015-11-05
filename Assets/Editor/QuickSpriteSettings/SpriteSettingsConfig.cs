0bc7f53a09329375499fe7db3462f00196e148c2
Edward Rowe <edward.l.rowe@gmail.com>
2015-11-05 10:25:43 -0500

    Saved SpriteSettings are now displayed as a reorderable list.

--- Assets/Editor/QuickSpriteSettings/SpriteSettingsConfig.cs
+++ Assets/Editor/QuickSpriteSettings/SpriteSettingsConfig.cs
@@ -27,14 +27,63 @@ namespace Staple.EditorScripts
     [CustomEditor(typeof(SpriteSettingsConfig))]
     public class SpriteSettingsConfigEditor : Editor
     {
+        private ReorderableList list;
+        private Editor textureSettingsEditor;
+
+        private void OnEnable() {
+            list = new ReorderableList(serializedObject, 
+                    serializedObject.FindProperty("SettingsSets"), 
+                    true, true, true, true);
+            list.drawHeaderCallback = (Rect rect) => {  
+                EditorGUI.LabelField(rect, "Saved SpriteSettings");
+            };
+        }
 
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
 
-            DrawDefaultInspector ();
+            list.DoLayoutList();
+            
+            EditorGUILayout.Space ();
+            EditorGUILayout.LabelField ("Selected Settings", EditorStyles.boldLabel);
+            
+            DrawSelectedSpriteSetting (serializedObject);
             
             serializedObject.ApplyModifiedProperties();
         }
+        
+        void DrawSelectedSpriteSetting (SerializedObject serializedObject)
+        {
+            int selectedIndex = list.index;
+            
+            SerializedProperty settingsSets = serializedObject.FindProperty ("SettingsSets");
+            
+            if (selectedIndex >= 0) {
+                EditorGUILayout.PropertyField (settingsSets.GetArrayElementAtIndex (selectedIndex));
+            } else {
+                EditorStyles.label.wordWrap = true;
+                if (GetPropertyArraySize (settingsSets) > 0) {
+                    EditorGUILayout.LabelField ("Select a Saved SpriteSetting to edit or remove it.");
+                } else {
+                    EditorGUILayout.LabelField ("No SpriteSettings have been saved. Create a new SpriteSetting from the list above.");
+                }
+            }
+        }
+        
+        int GetPropertyArraySize (SerializedProperty listAsProperty)
+		{
+			if (!listAsProperty.isArray) {
+				return -1;
+			}
+		
+			// Don't iterate on the original
+			SerializedProperty listCopy = listAsProperty.Copy ();
+		
+			listCopy.Next (true); // Skip generic element
+			listCopy.Next (true); // This is the array size
+		
+            return listCopy.intValue;
+		}
     }
 }
